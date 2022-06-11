56dc0789a1d3eaf466e6802c66e279786520e27e
Pablo Inoriza <pablo-ino@hotmail.com>
2022-06-11 14:59:47 -0300

    Base project

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,26 @@
+import Owner.SendNextItem
+import akka.NotUsed
+import akka.actor.typed.{ActorSystem, Behavior, Terminated}
+import akka.actor.typed.scaladsl.Behaviors
+
 object Main {
+
+  val itemsList: List[String] = List("Product1", "Product2", "Product3")
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    ActorSystem(Main(), "ChatRoomDemo")
   }
+
+  def apply(): Behavior[NotUsed] =
+    Behaviors.setup { context =>
+      val owner = context.spawn(Owner(itemsList, 3), "Owner")
+      context.watch(owner)
+
+      owner ! SendNextItem("sarasa")
+
+      Behaviors.receiveSignal {
+        case (_, Terminated(_)) =>
+          Behaviors.stopped
+      }
+    }
 }
