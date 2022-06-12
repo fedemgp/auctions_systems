1ab4f0582e0dad462be9c631bcc67785ff19c96e
Federico Gomez <fedemgp@gmail.com>
2022-06-12 02:18:45 -0300

    WIP: Making the communication bidirectional between Room and Owner

--- src/main/scala/Owner.scala
+++ src/main/scala/Owner.scala
@@ -8,23 +8,33 @@ object Owner {
   // private final case class PublishSessionMessage(screenName: String, message: String) extends RoomCommand
 
   sealed trait OwnerCommand
-  final case class SendNextItem(item: String) extends OwnerCommand
+  final case class SendNextItem(replyTo: ActorRef[AuctionItem]) extends OwnerCommand
 
   def apply(items: List[String], rooms: Int): Behavior[OwnerCommand] = {
     var currentItems = items
     Behaviors.setup { context =>
       for (room <- 1 to rooms) {
-        val roomMailBox = context.spawn(Room(), "room" + room)
+        val roomMailBox = context.spawn(new RoomSession(room)(), "room" + room)
         val init :+ last = currentItems
         roomMailBox ! AuctionItem(last, context.self)
         currentItems = init
       }
-      Behaviors.receive { (context, message) =>
-        message match {
-          case SendNextItem(item) =>
-            println("Empecé a subastar " + item)
-            Behaviors.stopped
-        }
+      sendItem(currentItems)
+    }
+  }
+
+  def sendItem(items: List[String]): Behavior[OwnerCommand] = {
+    Behaviors.receive { (context, message) =>
+      message match {
+        case SendNextItem(replyTo) =>
+          if (items.isEmpty) {
+            return Behaviors.stopped
+          } else {
+            val remainderItems :+ last = items
+            println("Comenzar a subastar item " + last)
+            replyTo ! AuctionItem(last, context.self)
+            return sendItem(remainderItems)
+          }
       }
     }
   }
--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,17 +1,25 @@
+import Owner.SendNextItem
+import Room.RoomCommand
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 
+class RoomSession(id: Int) {
+  def apply(): Behavior[RoomCommand] = {
+    Room(id)
+  }
+}
+
 object Room {
-
   sealed trait RoomCommand
-  final case class AuctionItem(item: String, replyTo: ActorRef[Nothing]) extends RoomCommand
+  final case class AuctionItem(item: String, replyTo: ActorRef[SendNextItem]) extends RoomCommand
 
-  def apply(): Behavior[RoomCommand] = Behaviors.receive { (context, message) => {
-      message match {
-        case AuctionItem(item, owner) =>
-          println("Empecé a subastar " + item)
-          Behaviors.same
-      }
+  def apply(id: Int): Behavior[RoomCommand] = Behaviors.receive { (context, message) => {
+    message match {
+      case AuctionItem(item, owner) =>
+        println("Room " + id + ": Subasté item  " + item)
+        owner ! SendNextItem(context.self)
+        Behaviors.same
     }
+  }
   }
 }
