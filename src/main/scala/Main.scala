210cf9582be6db351db5109e12eda42def3f0fb4
Federico Gomez <fedemgp@gmail.com>
2022-06-12 02:18:45 -0300

    WIP: Making the communication bidirectional between Room and Owner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,7 @@ import akka.actor.typed.scaladsl.Behaviors
 
 object Main {
 
-  val itemsList: List[String] = List("Product1", "Product2", "Product3")
+  val itemsList: List[String] = List("Product1", "Product2", "Product3", "Product4")
 
   def main(args: Array[String]): Unit = {
     ActorSystem(Main(), "ChatRoomDemo")
@@ -15,8 +15,6 @@ object Main {
     Behaviors.setup { context =>
       val owner = context.spawn(Owner(itemsList, 3), "Owner")
       context.watch(owner)
-
-      owner ! SendNextItem("sarasa")
 
       Behaviors.receiveSignal {
         case (_, Terminated(_)) =>
