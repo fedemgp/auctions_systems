d2d9fd85ac21124783aff9c0b06f604507089f38
Pablo Inoriza <pablo-ino@hotmail.com>
2022-07-01 01:17:18 -0300

    Aplico algunos refactors

--- src/main/scala/Owner.scala
+++ src/main/scala/Owner.scala
@@ -44,18 +44,16 @@ object Owner {
      * salas que lo soliciten el siguiente item a subastar. Cuando se quede sin items a subastar, enviará
      * el primer mensaje NoMoreItems, y cambiará su comportamiendo para ir cerrando las salas.
      */
-    Behaviors.receive { (context, message) =>
-      message match {
-        case SendNextItem(replyTo) =>
-          if (items.nonEmpty) {
-            val remainderItems :+ last = items
-            replyTo ! AuctionItem(last)
-            sendItem(remainderItems, rooms)
-          } else {
-            replyTo ! NoMoreItems()
-            endingProcess(1, rooms)
-          }
-      }
+    Behaviors.receiveMessage {
+      case SendNextItem(replyTo) =>
+        if (items.nonEmpty) {
+          val remainderItems :+ last = items
+          replyTo ! AuctionItem(last)
+          sendItem(remainderItems, rooms)
+        } else {
+          replyTo ! NoMoreItems()
+          endingProcess(1, rooms)
+        }
     }
   }
 
@@ -68,12 +66,10 @@ object Owner {
       println("Fin del owner")
       Behaviors.stopped
     } else {
-      Behaviors.receive { (context, message) =>
-        message match {
-          case SendNextItem(replyTo) =>
-            replyTo ! NoMoreItems()
-            endingProcess(notifiedRooms + 1, rooms)
-        }
+      Behaviors.receiveMessage {
+        case SendNextItem(replyTo) =>
+          replyTo ! NoMoreItems()
+          endingProcess(notifiedRooms + 1, rooms)
       }
     }
 
